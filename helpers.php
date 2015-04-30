2bfab1cb1645f6dcc63fcbc37d0ad31ea2993f27
Florian Steiner <fs@adiwidjaja.com>
2015-04-30 11:15:29 +0200

    Initial test commit.

---
+++ helpers.php
@@ -0,0 +1,31 @@
+<?php
+
+if ( ! function_exists('controller'))
+{
+ 	/**
+ 	 * Add a controller to routes
+ 	 *
+ 	 * @param string $uri
+ 	 * @param string $controller
+ 	 * @param array $names
+ 	 * @return void
+ 	 */   
+    function controller($uri, $controller, $names = [])
+    {
+        return app('router')->controller($uri, $controller, $names);
+    }
+}
+
+if ( ! function_exists('controllers'))
+{
+	/**
+	 * Add multiple controllers to routes
+	 *
+	 * @param array $controllers
+	 * @return void
+	 */
+	function controllers(array $controllers)
+	{
+		return app('router')->controllers($controllers);
+	}
+}
