1cd5e9722fe5805f11e9fada2494e84e9d55944e
Florian Steiner <fs@adiwidjaja.com>
2015-05-06 14:28:35 +0200

    Added blade extensions.

---
+++ BladeExtensions.php
@@ -0,0 +1,33 @@
+<?php namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Blade;
+
+class BladeExtenderServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		Blade::extend(function($view){
+			return preg_replace('/\@use\((.+)\)/', '<?php use ${1}; ?>', $view);
+		});
+		Blade::extend(function($view){
+			return preg_replace('/\@php\((.+)\)/', '<?php ${1} ?>', $view);
+		});
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+}
